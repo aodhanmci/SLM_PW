b586584eba5c76a587c711b3fb0d0e6ab0e7e79d
aodhanmci <35010870+aodhanmci@users.noreply.github.com>
2023-08-28 11:25:44 -0700

    updated gui
    gui now takes input on the exposure, gain and save file

--- gui.py
+++ gui.py
@@ -5,6 +5,8 @@ from pypylon import pylon
 from pypylon import genicam
 import os
 import time
+import matplotlib.pyplot as plt
+
 
 maxCamerasToUse = 2
 # get transport layer and all attached devices
@@ -30,14 +32,16 @@ def main():
 
     # define the window layout
     SLM_layout = [  [sg.Text('SLM')],
-                [sg.Image(filename='', key='SLM Image')]
+                [sg.Image(filename='', key='SLM Image')],
+                [sg.Button('Start'), sg.Button('Stop'), sg.Button('Exit')]
                 # [sg.Button('Upload Single'), sg.Button('1 loop'), sg.Button('5 loop')]
                 ]
     
     CCD_layout =  [  [sg.Text('CCD')],
                 [sg.Image(filename='', key='CCD Image')],
-                [sg.Button('Start'), sg.Button('Stop'), sg.Button('Exit')],
-                [sg.Button('save'), sg.Button('gain'), sg.Button('exposure')]
+                [sg.Text('save file', key='-SAVE_OUT-', font=('Arial Bold', 12), justification='center'), sg.Text('Exposure', key='-EXPOSURE_OUT-', font=('Arial Bold', 12), justification='center'), sg.Text('Gain', key='-GainE_OUT-', font=('Arial Bold', 12), justification='center')],
+                [sg.Input('', enable_events=True, key='-INPUT_SAVE-', font=('Arial Bold', 12), justification='left'), sg.Input('', enable_events=True, key='-INPUT_EXP-', font=('Arial Bold', 12), justification='left'), sg.Input('', enable_events=True, key='-INPUT_Gain-', font=('Arial Bold', 12), justification='left')],
+                [sg.Button('save'), sg.Button('exposure'), sg.Button('gain')]
                                 ]
     # layout = [[sg.VPush()],
     #           [sg.Push(), sg.Column(column_to_be_centered,element_justification='c'), sg.Push()],
@@ -60,15 +64,19 @@ def main():
     print("Using device ", camera.GetDeviceInfo().GetModelName())
 
     camera.Open()
-    camera.ExposureTimeRaw = 100000
-    camera.PixelFormat = "Mono12"
+    camera.ExposureTimeRaw = 5000
+    camera.GainRaw = 0
+    camera.PixelFormat = "Mono8"
+    print(camera.Width.GetValue())
     # camera.StartGrabbingMax(5000, pylon.GrabStrategy_LatestImageOnly)
     camera.StartGrabbing()
-    pylon.FeaturePersistence.Save("test.txt", camera.GetNodeMap())
+    # pylon.FeaturePersistence.Save("test.txt", camera.GetNodeMap())
 
     while True:
         event, values = window.read(timeout=20)
         if event == 'Exit' or event == sg.WIN_CLOSED:
+            window.close()
+            camera.Close()
             return
 
         elif event == 'Start':
@@ -81,23 +89,31 @@ def main():
             imgbytes = cv2.imencode('.png', img)[1].tobytes()
             window['CCD Image'].update(data=imgbytes)
             window['SLM Image'].update(data=imgbytes)
-            camera.Close()
- 
+            
+
         if running:
-            grabResult = camera.RetrieveResult(5000000, pylon.TimeoutHandling_ThrowException)
+            grabResult = camera.RetrieveResult(50000, pylon.TimeoutHandling_ThrowException)
             data = grabResult.GetArray()
-            data = cv2.imencode('.png', data)[1].tobytes()
+            imgdata = cv2.imencode('.png', data)[1].tobytes()
             img = np.full((500, 500), 255)
             imgbytes = cv2.imencode('.png', img)[1].tobytes()
+    
             # imgdata = data.tobytes()
             # imgbytes = cv2.imencode('.png', frame)[1].tobytes()  # ditto
 
-            window['CCD Image'].update(data)
-            # window['SLM Image'].update(imgbytes)
+            window['CCD Image'].update(imgdata)
+            window['SLM Image'].update(imgbytes)
+        
+        if event == 'save':
+            filename = values['-INPUT_SAVE-']
+            cv2.imwrite(f'{filename}.png', data)
+        if event == 'exposure':
+            camera.ExposureTimeRaw = int(values['-INPUT_EXP-'])
+        if event == 'gain':
+            camera.GainRaw = int(values['-INPUT_Gain-'])
 
-        # if event == 'Exposure':
-        #     running = True
-        #     camera.ExposureTime.SetValue(5000)
+        # 
+        # print
 
 
 
