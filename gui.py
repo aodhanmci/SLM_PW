d830fbffc03a53dfc6096eca41ed8e63102afe85
ajgonsalves <tony_g@outlook.com>
2023-09-08 13:33:09 -0700

    iniital gui creation

--- gui.py
+++ gui.py
@@ -1,51 +1,26 @@
+import PySimpleGUI as sg
 import cv2
 import numpy as np
+from pypylon import pylon
+from pypylon import genicam
 import os
+import time
+import matplotlib.pyplot as plt
+from PIL import Image, ImageTk
+import screeninfo
+from numpy import asarray
+import csv
 import pandas as pd
-import timeit
 import tkinter as tk
 from tkinter import filedialog, ttk
 from tkinter import messagebox
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from PIL import Image, ImageTk
-from pypylon import pylon
-from pypylon import genicam
-import screeninfo
-from numpy import asarray
-from SLM_TEST_GUI import *
-maxCamerasToUse = 2
 
-# get transport layer and all attached devices
-maximagestograb = 50
-tlf = pylon.TlFactory.GetInstance()
-devices = tlf.EnumerateDevices()
-NUM_CAMERAS = len(devices)
-os.environ["PYLON_CAMEMU"] = f"{NUM_CAMERAS}"
-exitCode = 0
-if NUM_CAMERAS == 0:
-    raise pylon.RuntimeException("No camera connected")
-else:
-    print(f'{NUM_CAMERAS} cameras detected:\n')
+# EDITED CODE AGAIN
+# THIS IS A TEST CHANGE BECAUSE GITHUB IS DUMB
 
-    for counter, device in enumerate(devices):
-        print(f'{counter}) {device.GetFriendlyName()}')  # return readable name
-        print(f'{counter}) {device.GetFullName()}\n')  # return unique code
 
-scale_percent = 60  # percent of original size
-width = int(1920 * scale_percent / 100)
-height = int(1200 * scale_percent / 100)
-dim = (width, height)
-
-screen_id = 1
-
-# get the size of the screen
-screen = screeninfo.get_monitors()[screen_id]
-# screen = screeninfo.get_monitors()
-print(screen)
-window_name = 'SLM'
-width, height = screen.width, screen.height
-
+width = 500
+height = 500
 root = tk.Tk()
 root.title("SLM CCD")
 root.geometry(f"{width}x{height}")
@@ -93,173 +68,280 @@ gain_entry.pack()
 save_label.pack()
 save_entry = tk.Entry(root)
 save_entry.pack()
+# maxCamerasToUse = 2
+# # get transport layer and all attached devices
+# maximagestograb = 50
+# tlf = pylon.TlFactory.GetInstance()
+# devices = tlf.EnumerateDevices()
+# NUM_CAMERAS = len(devices)
+# os.environ["PYLON_CAMEMU"] = f"{NUM_CAMERAS}"
+# exitCode = 0
+# if NUM_CAMERAS == 0:
+#     raise pylon.RuntimeException("No camera connected")
+# else:
+#     print(f'{NUM_CAMERAS} cameras detected:\n')
+    
+#     for counter, device in enumerate(devices):
+#         print(f'{counter}) {device.GetFriendlyName()}') # return readable name
+#         print(f'{counter}) {device.GetFullName()}\n') # return unique code
 
-# Create buttons for saving exposure, gain, and image
-save_exposure_button = tk.Button(root, text="Save Exposure")
-save_gain_button = tk.Button(root, text="Save Gain")
-save_slm_image_button = tk.Button(root, text="Save SLM Image")
+# scale_percent = 60 # percent of original size
+# width = int(1920 * scale_percent / 100)
+# height = int(1200 * scale_percent / 100)
+# dim = (width, height)
 
-save_exposure_button.pack()
-save_gain_button.pack()
-save_slm_image_button.pack()
+# screen_id = 1
 
-# Create canvas for Matplotlib plot
-canvas_frame = tk.Frame(root)
-canvas_frame.pack()
+# # get the size of the screen
+# screen = screeninfo.get_monitors()[screen_id]
+# # screen = screeninfo.get_monitors()
+# print(screen)
 
-# Define Matplotlib figure
-fig = Figure()
-ax = fig.add_subplot(111)
-ax.set_xlabel("X axis")
-ax.set_ylabel("Y axis")
-ax.grid()
-canvas = FigureCanvasTkAgg(fig, canvas_frame)
-canvas.get_tk_widget().pack(side='top', fill='both', expand=1)
 
-# Add event handlers for buttons
+# window_name = 'SLM'
+# width, height = screen.width, screen.height
+# cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+# cv2.moveWindow(window_name, screen.x - 1, screen.y - 1)
+# cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN,
+#                                 cv2.WINDOW_FULLSCREEN)
 
-def display_image(dataArray):
-    global slm_image_label
-    global slm_photo
-    frame = cv2.resize(dataArray, dim, interpolation=cv2.INTER_AREA)
-    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    slm_photo = ImageTk.PhotoImage(Image.fromarray(img))
-    slm_image_label.config(image=slm_photo)
-    slm_image_label.image = slm_photo
 
-def grab_ccd(camera):
-    grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
-    data = grabResult.GetArray()
-    global ccd_photo
-    frame = cv2.resize(data, dim, interpolation=cv2.INTER_AREA)
-    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    ccd_photo = ImageTk.PhotoImage(Image.fromarray(img))
-    ccd_image_label.config(image=ccd_photo)
-    ccd_image_label.image = ccd_photo
-    return data
 
-def clear_slm():
-    data_clear = np.zeros((1920, 1080))
-    display_image(data_clear)
 
-def on_start():
-    global running
-    running = True
-    clear_slm()
+# def draw_figure(canvas, figure, loc=(0, 0)):
+#     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+#     figure_canvas_agg.draw()
+#     figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+#     return figure_canvas_agg
 
-def on_stop(camera):
-    global running
-    running = False
-    df = pd.DataFrame({'exposure': [camera.ExposureTimeRaw.GetValue()],
-                       'gain': [camera.GainRaw.GetValue()]})
-    df.to_csv('prevVals.csv', index=False)
+# def displayImage(dataArray, window):
+#     frame = cv2.resize(dataArray, dim, interpolation=cv2.INTER_AREA)
+#     SLM_image = cv2.imencode('.png', frame)[1].tobytes()
+#     window['SLM Image'].update(SLM_image)
+#     cv2.imshow(window_name, dataArray)
 
-def on_save(camera, filename):
-    data = grab_ccd(camera)
-    cv2.imwrite(f'/Users/loasis/Documents/GitHub/SLM_PW/{filename}.png', data)
+# def grabCCD(camera, window):
+#     grabResult = camera.RetrieveResult(50000, pylon.TimeoutHandling_ThrowException)
+#     data = grabResult.GetArray()
 
-def on_exposure_change():
-    try:
-        camera.ExposureTimeRaw = int(exposure_entry.get())
-        exposure_entry.config(background="white")
-    except ValueError:
-        exposure_entry.config(background="red")
+#     frame = cv2.resize(data, dim, interpolation=cv2.INTER_AREA)
+#     imgdata = cv2.imencode('.png', frame)[1].tobytes()
 
-def on_gain_change():
-    try:
-        camera.GainRaw = int(gain_entry.get())
-        gain_entry.config(background="white")
-    except ValueError:
-        gain_entry.config(background="red")
+#     window['CCD Image'].update(imgdata)
 
-def on_upload_single():
-    filename = filedialog.askopenfilename()
-    if filename:
-        data_single_upload = cv2.imread(filename)
-        display_image(data_single_upload)
+#     return data
 
-def on_clear():
-    clear_slm()
+# def clearSLM(window):
+#     dataClear = np.zeros((1920,1080))
+#     displayImage(dataClear, window)
 
-def on_one_loop():
-    global threshold
-    resultImg, resultArray, diff = feedback(initialArray=data)
-    print("DIFF: " + str(np.round(diff, 2)))
-    display_image(resultArray)
-    resultImg.save('/Users/loasis/Documents/GitHub/SLM_PW/manualTestRESULT.png')
+# def main():
 
-def on_five_loop():
-    numLoops = 9
-    for i in np.arange(numLoops):
-        camera.StopGrabbing()
-        camera.StartGrabbingMax(1)
-        data = grab_ccd(camera)
-        if i == 0:
-            gratingImg, gratingArray, diff, threshold, allTest = feedback(
-                count=i,
-                plot=True,
-                initialArray=data
-            )
-        else:
-            gratingImg, gratingArray, diff, threshold, allTest = feedback(
-                count=i,
-                plot=True,
-                threshold=threshold,
-                initialArray=data
-            )
-        display_image(gratingArray)
-        root.update()
-        time.sleep(1)
-    camera.StartGrabbing()
+#     sg.theme('Black')
 
-def on_calibrate():
-    match = False
-    camera.StopGrabbing()
-    while not match:
-        xZoom = input("Enter xZoom: ")
-        yZoom = input("Enter yZoom: ")
-        xShift = input("Enter xShift: ")
-        yShift = input("Enter yShift: ")
-        angle = input("Enter angle: ")
-        data2 = calibration(data, float(xZoom), float(yZoom), float(xShift), float(yShift), float(angle))
-        data2Img = Image.fromarray(data2)
-        data2Img.show()
-        crosshairImg.show()
+#     df = pd.read_csv('prevVals.csv', usecols=['exposure','gain'])
 
-def on_save_slm_image():
-    gratingImg.save('/Users/loasis/Documents/GitHub/SLM_PW/SLMimage.png')
-    print("SLM image saved")
+#     # define the window layout
+#     SLM_layout = [  [sg.Text('SLM')],
+#                 [sg.Image(filename='', key='SLM Image')],
+#                 [sg.Button('Start'), sg.Button('Stop'), sg.Button('Exit')],
+#                 [sg.Button('Upload Single'), sg.FileBrowse(key="-SLM_Single-"), sg.Button('1 loop'), sg.Button('5 loop'), sg.Button('Clear'), sg.Button('Calibrate')]
+#                 ]
+    
+#     CCD_layout =  [  [sg.Text('CCD')],
+#                 [sg.Image(filename='', key='CCD Image')],
+#                 [sg.Text('save file', key='-SAVE_OUT-', font=('Arial Bold', 12), justification='center'), sg.Text('Exposure', key='-EXPOSURE_OUT-', font=('Arial Bold', 12), justification='center'), sg.Text('Gain', key='-GainE_OUT-', font=('Arial Bold', 12), justification='center')],
+#                 [sg.Input('', enable_events=True, key='-INPUT_SAVE-', font=('Arial Bold', 12), justification='left'), sg.Input(str(df.exposure[0]), enable_events=True, key='-INPUT_EXP-', font=('Arial Bold', 12), justification='left'), sg.Input(str(df.gain[0]), enable_events=True, key='-INPUT_Gain-', font=('Arial Bold', 12), justification='left')],
+#                 [sg.Button('save'), sg.Button('exposure'), sg.Button('gain')]
+#                                 ]
+#     # layout = [[sg.VPush()],
+#     #           [sg.Push(), sg.Column(column_to_be_centered,element_justification='c'), sg.Push()],
+#     #           [sg.VPush()]]
 
-start_button.config(command=on_start)
-stop_button.config(command=lambda: on_stop(camera))
-exit_button.config(command=root.destroy)
-upload_single_button.config(command=on_upload_single)
-one_loop_button.config(command=on_one_loop)
-five_loop_button.config(command=on_five_loop)
-clear_button.config(command=on_clear)
-calibrate_button.config(command=on_calibrate)
-save_exposure_button.config(command=on_exposure_change)
-save_gain_button.config(command=on_gain_change)
-save_slm_image_button.config(command=on_save_slm_image)
+#     layout =   [  [sg.Column(SLM_layout),
+#      sg.VSeperator(),
+#      sg.Column(CCD_layout)]]
+#     # create the window and show it without the plot
+#     window = sg.Window('SLM CCD',
+#                        layout, location=(100, 100), resizable=False)
 
-image_window = pylon.PylonImageWindow()
-image_window.Create(1)
-# Create an instant camera object with the camera device found first.
-camera = pylon.InstantCamera(tlf.CreateDevice(devices[0]))
-running = False
-# Print the model name of the camera.
-print("Using device ", camera.GetDeviceInfo().GetModelName())
+# # second window  things
+#     # layout2 = [[sg.Text('Animated Matplotlib', size=(40, 1),
+#     #             justification='center', font='Helvetica 20')],
+#     #           [sg.Canvas(size=(640, 480), key='-CANVAS-')]
+#     # ]
 
-camera.Open()
-# default acquisition attributes when opening the camera
-# camera.ExposureTimeRaw = 180000
-# camera.GainRaw = 190
-camera.PixelFormat = "Mono8"
-# take previously set values as default
-camera.ExposureTimeRaw = int(100)
-camera.GainRaw = int(0)
-# camera.StartGrabbingMax(5000, pylon.GrabStrategy_LatestImageOnly)
-camera.StartGrabbing()
-# pylon.FeaturePersistence.Save("test.txt", camera.GetNodeMap())
+#     # # create the form and show it without the plot
+#     # window2 = sg.Window('Demo Application - Embedding Matplotlib In PySimpleGUI',
+#     #             layout2, finalize=True)
 
-root.mainloop()
+#     # canvas_elem = window2['-CANVAS-']
+#     # canvas = canvas_elem.TKCanvas
+
+#     # # draw the initial plot in the window
+#     # fig = Figure()
+#     # ax = fig.add_subplot(111)
+#     # ax.set_xlabel("X axis")
+#     # ax.set_ylabel("Y axis")
+#     # ax.grid()
+#     # fig_agg = draw_figure(canvas, fig)
+
+
+#     # ---===--- Event LOOP Read and display frames, operate the GUI --- #
+#     imageWindow = pylon.PylonImageWindow()
+#     imageWindow.Create(1)
+#         # Create an instant camera object with the camera device found first.
+#     camera = pylon.InstantCamera(tlf.CreateDevice(devices[0]))
+#     running=False
+#     # Print the model name of the camera.
+#     print("Using device ", camera.GetDeviceInfo().GetModelName())
+
+
+#     camera.Open()
+#     # default acquisition attributes when opening the camera
+#     # camera.ExposureTimeRaw = 180000
+#     # camera.GainRaw = 190
+#     camera.PixelFormat = "Mono8"
+#     # take previously set values as default
+#     camera.ExposureTimeRaw = int(df.exposure[0])
+#     camera.GainRaw = int(df.gain[0])
+#     # camera.StartGrabbingMax(5000, pylon.GrabStrategy_LatestImageOnly)
+#     camera.StartGrabbing()
+#     # pylon.FeaturePersistence.Save("test.txt", camera.GetNodeMap())
+
+
+
+
+
+#     while True:
+#         event, values = window.read(timeout=20)
+#         if event == 'Exit' or event == sg.WIN_CLOSED:
+#             window.close()
+#             camera.Close()
+#             return
+
+#         elif event == 'Start':
+#             running = True
+#             clearSLM(window)
+
+#         elif event == 'Stop':
+#             running = False
+
+#             # cv2.destroyWindow('SLM')
+
+#             df = pd.DataFrame({'exposure': [camera.ExposureTimeRaw.GetValue()],
+#                                'gain': [camera.GainRaw.GetValue()]})
+#             df.to_csv('prevVals.csv', index=False)
+            
+
+#         if running:
+#             data = grabCCD(camera, window)
+        
+#         if event == 'save':
+#             filename = values['-INPUT_SAVE-']
+#             cv2.imwrite(f'/Users/loasis/Documents/GitHub/SLM_PW/{filename}.png', data)
+#         if event == 'exposure':
+#             try:
+#                 camera.ExposureTimeRaw = int(values['-INPUT_EXP-'])
+#                 window['-INPUT_EXP-'].update(background_color=sg.theme_input_background_color())
+#             except Exception as error:
+#                 window['-INPUT_EXP-'].update(background_color='red')
+#                 # print(error)
+#                 print("Exposure value not accepted. Please input a value above 34.")
+#         if event == 'gain':
+#             # camera.GainRaw = int(values['-INPUT_Gain-'])
+#             try:
+#                 camera.GainRaw = int(values['-INPUT_Gain-'])
+#                 window['-INPUT_Gain-'].update(background_color=sg.theme_input_background_color())
+#             except Exception as error:
+#                 window['-INPUT_Gain-'].update(background_color='red')
+#                 # window['-INPUT_Gain-'].Widget.configure(highlightcolor='red', highlightthickness=2)
+#                 # print(error)
+#                 print("Gain value not accepted. Please input a value less than 360.")
+#         if event == 'Upload Single':
+#             SLM_image = values["-SLM_Single-"]
+#             print(SLM_image)
+#             data_single_upload = cv2.imread(SLM_image)
+#             displayImage(data_single_upload, window)
+            
+#             # cv2.waitKey()
+#         if event == 'Clear':
+#             clearSLM(window)
+#         if event == '1 loop':
+#             resultImg, resultArray, diff = feedback(initialArray = data)
+
+#             print("DIFF: " + str(np.round(diff,2)))
+#             displayImage(resultArray, window)
+
+#             resultImg.save('/Users/loasis/Documents/GitHub/SLM_PW/manualTestRESULT.png')
+
+#             # SLM_grating_img = Image.fromarray(resultArray, 'L')
+#             # # data = cv2.imread(resultImg)
+#             # data = cv2.imread(SLM_grating_img)
+#             # frame = cv2.resize(data, dim, interpolation=cv2.INTER_AREA)
+#             # SLM_image = cv2.imencode('.png', frame)[1].tobytes()
+#             # window['SLM Image'].update(SLM_image)
+#         if event == '5 loop':
+#             numLoops = 9
+#             for i in np.arange(numLoops):
+#                 camera.StopGrabbing()
+#                 # data = grabCCD(camera, window)
+#                 # print("RUNNING")
+#                 # try:
+#                 camera.StartGrabbingMax(1)
+#                 data = grabCCD(camera, window)
+#                 if i == 0:
+#                     gratingImg, gratingArray, diff, threshold, allTest = feedback(
+#                         count = i,
+#                         plot = True,
+#                         # threshold = 175,
+#                         initialArray = data
+#                     )
+#                 else:
+#                     gratingImg, gratingArray, diff, threshold, allTest = feedback(
+#                         count = i,
+#                         plot = True,
+#                         threshold = threshold,
+#                         initialArray = data
+#                     )
+#                 displayImage(gratingArray, window)
+#                 window.refresh()
+#                 time.sleep(1)
+                
+
+#                 # data = grabCCD(camera, window)
+
+#                 # gratingImg.show()
+#                 # print("DIFF: " + str(np.round(diff,2)))
+#                 # data = grabCCD(camera, window)
+
+#                 window.refresh()
+               
+#                 if i == int(numLoops-1):
+#                     camera.StartGrabbing()
+
+#         if event == 'Calibrate':
+#             match = False
+#             camera.StopGrabbing()
+#             while match == False:
+
+#                 xZoom = input("Enter xZoom: ")
+#                 yZoom = input("Enter yZoom: ")
+#                 xShift = input("Enter xShift: ")
+#                 yShift = input("Enter yShift: ")
+#                 angle = input("Enter angle: ")
+
+#                 data2 = calibration(data, float(xZoom), float(yZoom), float(xShift), float(yShift), float(angle))
+
+#                 data2Img = Image.fromarray(data2)
+#                 data2Img.show()
+#                 crosshairImg.show()
+        
+#         if event == "Save SLM Image":
+#             gratingImg.save('/Users/loasis/Documents/GitHub/SLM_PW/SLMimage.png')
+#             print("SLM image saved")
+
+
+
+
+# main()
