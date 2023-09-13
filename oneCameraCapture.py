736054886b9c4460f6cc5e7be0a73a70150aa0f8
ajgonsalves <tony_g@outlook.com>
2023-09-13 16:33:30 -0700

    tkinter progress

--- oneCameraCapture.py
+++ oneCameraCapture.py
@@ -2,67 +2,72 @@ import os
 
 os.environ["PYLON_CAMEMU"] = "3"
 
-# from pypylon import genicam
-# from pypylon import pylon
+from pypylon import genicam
+from pypylon import pylon
 import sys
 import time
 import cv2
 import numpy as np
 import tkinter as tk
+from tkinter import ttk, PhotoImage, filedialog
+from tkinter.filedialog import askopenfile
 import video_display
+from PIL import Image, ImageTk
 
 class cameraCapture(tk.Frame):
-    
+
     def __init__(self, page_instance):
         self.img0 = []
         self.windowName = 'SLM CCD'
         self.page = page_instance
-        # try:
-        #     # Create an instant camera object with the camera device found first.
-        #     # maxCamerasToUse = 2
-        #     # get transport layer and all attached devices
-        #     tlf = pylon.TlFactory.GetInstance()
-        #     devices = tlf.EnumerateDevices()
-        #     NUM_CAMERAS = len(devices)
-        #     os.environ["PYLON_CAMEMU"] = f"{NUM_CAMERAS}"
-        #     exitCode = 0
-        #     if NUM_CAMERAS == 0:
-        #         raise pylon.RuntimeException("No camera connected")
-        #     else:
-        #         print(f'{NUM_CAMERAS} cameras detected:\n')
+        # self.SLMdilp = Image.open('10lpmm_190amp.png')
+        self.SLMdisp = Image.fromarray(np.zeros((1080,1920)))
+        try:
+            # Create an instant camera object with the camera device found first.
+            # maxCamerasToUse = 2
+            # get transport layer and all attached devices
+            tlf = pylon.TlFactory.GetInstance()
+            devices = tlf.EnumerateDevices()
+            NUM_CAMERAS = len(devices)
+            os.environ["PYLON_CAMEMU"] = f"{NUM_CAMERAS}"
+            exitCode = 0
+            if NUM_CAMERAS == 0:
+                raise pylon.RuntimeException("No camera connected")
+            else:
+                print(f'{NUM_CAMERAS} cameras detected:\n')
                 
-        #         for counter, device in enumerate(devices):
-        #             print(f'{counter}) {device.GetFriendlyName()}') # return readable name
-        #             print(f'{counter}) {device.GetFullName()}\n') # return unique code
-        #     self.camera = pylon.InstantCamera(tlf.CreateDevice(devices[0]))
-        #     running=False
-        #     # Print the model name of the camera.
-        #     print("Using device ", self.camera.GetDeviceInfo().GetModelName())
+                for counter, device in enumerate(devices):
+                    print(f'{counter}) {device.GetFriendlyName()}') # return readable name
+                    print(f'{counter}) {device.GetFullName()}\n') # return unique code
+            self.camera = pylon.InstantCamera(tlf.CreateDevice(devices[0]))
+            running=False
+            # Print the model name of the camera.
+            print("Using device ", self.camera.GetDeviceInfo().GetModelName())
 
-        #     # self.camera.PixelFormat = "Mono8"
+            # self.camera.PixelFormat = "Mono8"
        
-        #     self.camera.Open()  #Need to open camera before can use camera.ExposureTime
-        #     # self.camera.PixelFormat = "Mono8"
-        #     self.camera.ExposureTimeRaw = 1000
+            self.camera.Open()  #Need to open camera before can use camera.ExposureTime
+            # self.camera.PixelFormat = "Mono8"
+            self.camera.ExposureTimeRaw = 1000
 
-        #     # Print the model name of the camera.
-        #     # print("Using device ", self.camera.GetDeviceInfo().GetModelName())
-        #     # print("Exposure time ", self.camera.ExposureTime.GetValue())
+            # Print the model name of the camera.
+            # print("Using device ", self.camera.GetDeviceInfo().GetModelName())
+            # print("Exposure time ", self.camera.ExposureTime.GetValue())
 
-        #     # According to their default configuration, the cameras are
-        #     # set up for free-running continuous acquisition.
-        #     #Grabbing continuously (video) with minimal delay
-        #     self.camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly) 
+            # According to their default configuration, the cameras are
+            # set up for free-running continuous acquisition.
+            #Grabbing continuously (video) with minimal delay
+            self.camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly) 
 
-        #     # converting to opencv bgr format
-        #     self.converter = pylon.ImageFormatConverter()
-        #     self.converter.OutputPixelFormat = pylon.PixelType_Mono8
-        #     self.converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
+            # converting to opencv bgr format
+            self.converter = pylon.ImageFormatConverter()
+            self.converter.OutputPixelFormat = pylon.PixelType_Mono8
+            self.converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
 
-        # except genicam.GenericException as e:
-        #     # Error handling
-        #     print("An exception occurred.", e.GetDescription())
-        #     exitCode = 1
+        except genicam.GenericException as e:
+            # Error handling
+            print("An exception occurred.", e.GetDescription())
+            exitCode = 1
 
     def getFrame(self):
         try:
@@ -84,27 +89,67 @@ class cameraCapture(tk.Frame):
             # Error handling
             print("An exception occurred.", e.GetDescription())
             exitCode = 1
+
     def exposure_change(self):
         try:
             self.camera.ExposureTimeRaw = int(self.page.exposure_entry.get())
             self.page.exposure_entry.config(background="white")
-        except ValueError:
+        except Exception as error:
+            print(error)
             self.page.exposure_entry.config(background="red")
 
     def gain_change(self):
         try:
             self.camera.GainRaw = int(self.page.gain_entry.get())
             self.page.gain_entry.config(background="white")
-        except ValueError:
+        except Exception as error:
+            print(error)
             self.page.gain_entry.config(background="red")
+
     def save_image(self):
         filename = self.page.save_entry.get()
         try:
             cv2.imwrite(f'{filename}.png', self.img0)  # Save the captured image to a file
             print(f"Image saved as {filename}")
-        except ValueError:
+        except Exception as error:
+            print(error)
             self.page.gain_entry.config(background="red")
+    
+    def browse(self):
+        global img
+        f_types = [('PNG files', '*.png')]
+        filename = filedialog.askopenfilename(filetypes=f_types)
+        # self.browseImg = ImageTk.PhotoImage(file=filename)
+        self.browseImg = Image.open(filename)
+        # img_width = img.width()
+        # img_height = img.height()
+        self.browseImgArray = np.asarray(self.browseImg)
+        # print(self.browseImgArray)
+        # self.browseImgArray = self.browseImgArray.astype(np.uint8)
+        # return self.browseResultArray
 
+    def displayToSLM(self):
+        # try:
+        testImg = Image.open("test.png")
+        # print(type(testImg))
+        # testImg = cv2.resize(testImg, dsize = (self.page.SLMdim[0], self.page.SLMdim[1]))
+        # testImg.show()
+        # testLabel = tk.Label(self.window, testImg)
+        # self.SLMdisp = Image.fromarray(np.uint8(self.browseImgArray), "L")
+        # print(type(Image.fromarray(self.browseImgArray)))
+        self.SLMdisp = Image.fromarray(self.browseImgArray)
+        # print(type(self.SLMdisp))
+        # print(self.SLMdisp.mode)
+        # self.SLMdisp = testImg
+        # print(type(self.SLMdisp))
+        # print(self.SLMdisp.mode)
+        # print("DONE")
+        # except Exception as error:
+        #     print(error)
+        # Image.fromarray(self.browseResultArray).show()
+
+    def clearSLM(self):
+        self.SLMdisp = Image.fromarray(np.zeros((1080,1920)))
 
 
 if __name__ == "__main__":
--- video_display.py
+++ video_display.py
@@ -1,13 +1,15 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, PhotoImage, filedialog
+from tkinter.filedialog import askopenfile
 from tkinter.constants import *
-from tkinter import PhotoImage
 import oneCameraCapture
 from PIL import Image, ImageTk
 import numpy as np
-from tkinter import PhotoImage
 import cv2
 import PIL
+import SLM_TEST_GUI
+import screeninfo
+from screeninfo import get_monitors
 
 small_button_height = 20
 small_button_width = 50
@@ -20,13 +22,13 @@ button_gap = 0
 window_width = 1500
 window_height = 600
 
-scale_percent = 40 # percent of original size
+scale_percent = 30 # percent of original size
 width_scale = int(window_width * scale_percent / 100)
 height_scale = int(window_height * scale_percent / 100)
 dim = (width_scale, height_scale)
-print(dim)
-second_row_button_height = window_height-2*large_button_height-button_gap
-first_row_button_height = window_height-large_button_height
+# print(dim)
+first_row_button_height = window_height-2*large_button_height-2*button_gap
+second_row_button_height = window_height-large_button_height-button_gap
 
 upper_row_dict = {"y":first_row_button_height, "height":large_button_height, "width":large_button_width}
 lower_row_dict = {"y":second_row_button_height, "height":large_button_height, "width":large_button_width}
@@ -36,9 +38,11 @@ class Page(tk.Frame):
     def __init__(self, parent, window):
         
         tk.Frame.__init__(self, parent)
-        self.window = window
-        self.window.title = "SLM CCD"
 
+        # super(Page, self).__init__()
+
+        self.window = window
+        window.title("SLM & CCD Control")
         window.geometry(f"{window_width}x{window_height}")
 
         # Create a label for the SLM image
@@ -61,38 +65,46 @@ class Page(tk.Frame):
         self.exit_button = tk.Button(window, text="Exit")
         self.exit_button.place(x=2*large_button_width, **upper_row_dict)
 
-        self.upload_single_button = tk.Button(window, text="Upload Single")
-        self.upload_single_button.place(x=0, **lower_row_dict)
+        self.browse_button = tk.Button(window, text="Browse", command=self.vid.browse)
+        self.browse_button.place(x=0, **lower_row_dict)
+        self.display_button = tk.Button(window, text="Display to SLM", command=self.vid.displayToSLM)
+        self.display_button.place(x=1*large_button_width, **lower_row_dict)
         self.one_loop_button = tk.Button(window, text="1 loop")
-        self.one_loop_button.place(x=large_button_width, **lower_row_dict)
+        self.one_loop_button.place(x=2*large_button_width, **lower_row_dict)
         self.five_loop_button = tk.Button(window, text="5 loop")
-        self.five_loop_button.place(x=2*large_button_width, **lower_row_dict)
-        self.clear_button = tk.Button(window, text="Clear")
-        self.clear_button.place(x=3*large_button_width, **lower_row_dict)
+        self.five_loop_button.place(x=3*large_button_width, **lower_row_dict)
+        self.clear_button = tk.Button(window, text="Clear", command=self.vid.clearSLM)
+        self.clear_button.place(x=4*large_button_width, **lower_row_dict)
         self.calibrate_button = tk.Button(window, text="Calibrate")
-        self.calibrate_button.place(x=4*large_button_width, **lower_row_dict)
+        self.calibrate_button.place(x=6*large_button_width, **lower_row_dict)
 
         # Create labels and entry widgets for exposure, gain, and save file
         self.exposure_button = tk.Button(window, text="Set Exposure", command=self.vid.exposure_change)
-        self.exposure_button.place(x=window_width-3*large_button_width, **upper_row_dict)
+        self.exposure_button.place(x=window_width-3*large_button_width, **lower_row_dict)
         self.gain_button = tk.Button(window, text="Set Gain", command=self.vid.gain_change)
-        self.gain_button.place(x=window_width-2*large_button_width, **upper_row_dict)
+        self.gain_button.place(x=window_width-2*large_button_width, **lower_row_dict)
         self.save_button = tk.Button(window, text="Save File", command=self.vid.save_image)
-        self.save_button.place(x=window_width-large_button_width, **upper_row_dict)
+        self.save_button.place(x=window_width-large_button_width, **lower_row_dict)
 
         self.exposure_entry = tk.Entry(window)
-        self.exposure_entry.place(x=window_width-3*large_button_width, **lower_row_dict)
+        self.exposure_entry.place(x=window_width-3*large_button_width, **upper_row_dict)
         self.gain_entry = tk.Entry(window)
-        self.gain_entry.place(x=window_width-2*large_button_width, **lower_row_dict)
+        self.gain_entry.place(x=window_width-2*large_button_width, **upper_row_dict)
         self.save_entry = tk.Entry(window)
-        self.save_entry.place(x=window_width-1*large_button_width, **lower_row_dict)
-        #Open camera source
+        self.save_entry.place(x=window_width-1*large_button_width, **upper_row_dict)
         
+        # Detect SLM monitor
+        self.SLMdisplayNum = 1
+        self.SLMdim = (int(get_monitors()[self.SLMdisplayNum].width), int(get_monitors()[self.SLMdisplayNum].height))
+        # print(self.SLMdim)     # width = 1280, height = 1024
+
+
         #Create a canvas that will fit the camera source
-        self.SLM_image_widget = tk.Label(window, width=width_scale, height=height_scale)
+        self.SLM_image_widget = tk.Label(window, width=int(self.SLMdim[1]*scale_percent/100), height=int(self.SLMdim[0]*scale_percent/100))
         self.SLM_image_widget.place(x=1*window_width/4, y=window_height/2, anchor=tk.CENTER)
 
-        self.ccd_image_widget = tk.Label(window, width=width_scale, height=height_scale)
+        # self.ccd_image_widget = tk.Label(window, width=width_scale, height=height_scale)
+        self.ccd_image_widget = tk.Label(window, width=int(self.vid.getFrame().shape[1]*scale_percent/100), height=int(self.vid.getFrame().shape[0]*scale_percent/100))
         self.ccd_image_widget.place(x=3*window_width/4, y=window_height/2, anchor=tk.CENTER)
 
         self.delay=10
@@ -104,25 +116,28 @@ class Page(tk.Frame):
         #Get a frame from cameraCapture
 
         # Example arrays (you can replace these with your actual image data)
-        image_array1 = np.random.randint(0, 256, size=(width_scale, height_scale), dtype=np.uint8).T
-        image_array2 = np.random.randint(0, 10, size=(width_scale, height_scale), dtype=np.uint8).T
+        # image_array1 = np.random.randint(0, 256, size=(int(self.SLMdim[0]*scale_percent/100), int(self.SLMdim[1]*scale_percent/100)), dtype=np.uint8).T
+        # image_array2 = np.random.randint(0, 10, size=(width_scale, height_scale), dtype=np.uint8).T
         # Convert NumPy arrays to Pillow Images
-        image1 = Image.fromarray(image_array1)
-        image2 = Image.fromarray(image_array2)
+        # image1 = Image.fromarray(image_array1)
+        # image2 = Image.fromarray(image_array2)
 
-        photo1 = PIL.ImageTk.PhotoImage(image=image1)
+        photo1 = np.asarray(self.vid.SLMdisp)
+        # print(type(photo1))
+        photo1 = cv2.resize(photo1, dsize=(int(self.SLMdim[1]*scale_percent/100), int(self.SLMdim[0]*scale_percent/100)))
+        photo1 = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(photo1))
         self.SLM_image_widget.photo = photo1
         self.SLM_image_widget.config(image=photo1)
 
-        photo2 = PIL.ImageTk.PhotoImage(image=image2)
-        self.ccd_image_widget.photo = photo2
-        self.ccd_image_widget.config(image=photo2)
+        # photo2 = PIL.ImageTk.PhotoImage(image=image2)
+        # self.ccd_image_widget.photo = photo2
+        # self.ccd_image_widget.config(image=photo2)
 
-        # ccd_data = self.vid.getFrame() #This is an array
-        # ccd_data = cv2.resize(ccd_data, dsize=(width_scale, height_scale), interpolation=cv2.INTER_CUBIC)
-        # self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(ccd_data))
-        # self.ccd_image_widget.config(image=self.photo)
-        # self.ccd_image_widget.photo = self.photo
+        self.ccd_data = self.vid.getFrame() #This is an array
+        self.ccd_data = cv2.resize(self.ccd_data, dsize=(int(self.vid.getFrame().shape[1]*scale_percent/100), int(self.vid.getFrame().shape[0]*scale_percent/100)), interpolation=cv2.INTER_CUBIC)
+        self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(self.ccd_data))
+        self.ccd_image_widget.config(image=self.photo)
+        self.ccd_image_widget.photo = self.photo
 
         self.window.after(self.delay, self.update)
 
