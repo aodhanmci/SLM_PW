8d71bb4545aa1bf558721cbeeb18da98544ba5d8
aodhanmci <35010870+aodhanmci@users.noreply.github.com>
2024-02-02 17:25:28 -0800

    EOD 02.02.24

--- GS.py
+++ GS.py
@@ -1,11 +1,12 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import numpy.fft as fft
+from PIL import Image
 
 ### Define constants
 w_i = 3.24*10**-3  # input waist
 # w_i = 3.24*10**-3
-w_o =700*10**-6  # output waist
+w_o = 960*10**-6  # output waist
 wavelength = 632*10**-9  # wavelength
 SLM_h, SLM_w = 512, 512  # SLM resolution
 SLM_pixelsize = 8*10**-6  # SLM pixel size
@@ -33,7 +34,7 @@ def gs(img, target, initial_pm, max_iter):
         E_out = fft.fftshift(fft.ifft2(fft.fftshift(E_cor)))
 
     pm = np.angle(E_out)
-    # pm = np.where(pm < 0, 2*np.pi+pm, pm)
+    pm = np.where(pm < 0, 2*np.pi+pm, pm)
 
     return pm
 
@@ -41,24 +42,71 @@ def gs(img, target, initial_pm, max_iter):
 def rmse(result, target):
     return np.sqrt(np.sum(np.square(result-target))/np.sum(np.square(target)))
 
+file = r'C:\Users\loasis\Documents\GitHub\SLM_PW\Tests\Diode\original.png'
+image = np.array(Image.open(file))
 
-x = np.linspace(-0.005, 0.005, 512)
-y = np.linspace(-0.005, 0.005, 512)
+x = np.linspace(-0.005, 0.005, 1600)
+y = np.linspace(-0.005, 0.005, 1200)
 X, Y = np.meshgrid(x, y)
 
-x2 = np.linspace(-255, 256, 512)
-y2 = np.linspace(-255, 256, 512)
+x2 = np.linspace(-255, 256, 1600)
+y2 = np.linspace(-255, 256, 1200)
 X2, Y2 = np.meshgrid(x2, y2)
 
 input_beam = np.exp(-2*(X**2+Y**2)/w_i**2)
 output_beam = np.exp(-2*(X/w_o)**40-2*(Y/w_o)**40)
 
-# pm = gs(input_beam, output_beam, 0.004*(X2**2+Y2**2), 200)
-# # pmN = pm/2/np.pi*255
+pm = gs(image, output_beam, 0.0025*(X2**2+Y2**2), 200)
+pmN = pm/2/np.pi*255
 # pmN = (pm+np.pi)/2/np.pi*255
-# E_f = fft.fftshift(fft.fft2(fft.fftshift(np.sqrt(input_beam)*np.exp(pm * 1j))))
-# recovery = np.absolute(E_f)
-# recoveryN = recovery/np.amax(recovery)
+E_f = fft.fftshift(fft.fft2(fft.fftshift(np.sqrt(image)*np.exp(pm * 1j))))
+recovery = np.absolute(E_f)
+recoveryN = recovery/np.amax(recovery)
+
+fig = plt.figure()
+
+ax1 = fig.add_subplot(221)
+ax1.imshow(image, cmap='gray')
+
+ax2 = fig.add_subplot(222)
+ax2.imshow(output_beam*255, cmap='gray')
+
+ax3 = fig.add_subplot(223)
+ax3.imshow(pmN, cmap='gray')
+
+ax4 = fig.add_subplot(224)
+ax4.imshow(np.uint8(recoveryN*255), 'gray')
+
+plt.show()
+
+fig3 = plt.figure()
+ax = fig3.add_subplot(111)
+ax.imshow(pmN, cmap='gray')
+ax.axis('off')
+fig3.savefig('phasemap.png')
+
+#
+# fig2 = plt.figure()
+# ax = fig2.add_subplot(111)
+# ax.plot(x2, 255*recoveryN[256, :])
+#
+# fig2.show()
+
+# rmse_list = []
+#
+# for i in np.arange(c1, c2, 0.0001):
+#     pm = gs(input_beam, output_beam, 0.004 * (X2 ** 2 + Y2 ** 2), 100)
+#     pmN = (pm + np.pi) / 2 / np.pi * 255
+#     E_f = fft.fftshift(fft.fft2(fft.fftshift(np.sqrt(input_beam) * np.exp(pm * 1j))))
+#     recovery = np.absolute(E_f)
+#     recoveryN = recovery/np.amax(recovery)
+#     rmse_list.append(rmse(recoveryN[220:300], output_beam[220:300]))
+#
+# fig2 = plt.figure()
+# ax5 = fig2.add_subplot(111)
+# ax5.plot(np.arange(c1, c2, 0.0001), rmse_list)
+#
+# plt.show()
 #
 # fig = plt.figure()
 #
@@ -74,44 +122,6 @@ output_beam = np.exp(-2*(X/w_o)**40-2*(Y/w_o)**40)
 # ax4 = fig.add_subplot(224)
 # ax4.imshow(np.uint8(recoveryN*255), 'gray')
 #
-# fig.show()
+# plt.show()
 #
-# fig2 = plt.figure()
-# ax = fig2.add_subplot(111)
-# ax.plot(x2, 255*recoveryN[256, :])
-#
-# fig2.show()
-
-rmse_list = []
-
-for i in np.arange(c1, c2, 0.0001):
-    pm = gs(input_beam, output_beam, 0.004 * (X2 ** 2 + Y2 ** 2), 300)
-    pmN = (pm + np.pi) / 2 / np.pi * 255
-    E_f = fft.fftshift(fft.fft2(fft.fftshift(np.sqrt(input_beam) * np.exp(pm * 1j))))
-    recovery = np.absolute(E_f)
-    recoveryN = recovery/np.amax(recovery)
-    rmse_list.append(rmse(recoveryN[220:300], output_beam[220:300]))
-
-fig2 = plt.figure()
-ax5 = fig2.add_subplot(111)
-ax5.plot(np.arange(c1, c2, 0.0001), rmse_list)
-
-fig2.show()
-
-fig = plt.figure()
-
-ax1 = fig.add_subplot(221)
-ax1.imshow(input_beam*255, cmap='gray')
-
-ax2 = fig.add_subplot(222)
-ax2.imshow(output_beam*255, cmap='gray')
-
-ax3 = fig.add_subplot(223)
-ax3.imshow(pmN, cmap='gray')
-
-ax4 = fig.add_subplot(224)
-ax4.imshow(np.uint8(recoveryN*255), 'gray')
-
-fig.show()
-
-print()
+# print()
